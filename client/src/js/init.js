1ef311bffffa5243ace7690010ee1ff1b82974aa
elynch12 <elynch@control2k.com>
2020-01-13 23:53:51 +0000

    Moved inline styling to init.js

--- client/src/js/init.js
+++ client/src/js/init.js
@@ -4,6 +4,6 @@
     $('.sidenav').sidenav();
     $('.modal').modal();
     $('.tabs').tabs();
-
+    $('.collapsible').collapsible();
   }); // end of document ready
 })(jQuery); // end of jQuery name space

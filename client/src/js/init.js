8f56c6c745d186710e4abeeb880c31eaf19b6962
Edward Lynch <edwich44@gmail.com>
2020-08-06 15:48:29 +0100

    Added minified files

--- client/src/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.modal').modal();
-    $('.tabs').tabs();
-    $('.collapsible').collapsible();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
